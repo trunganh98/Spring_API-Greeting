3a9717043f89cbe2d0fe7ee8bf59b466fd588f94
trunganh98 <60956094+trunganh98@users.noreply.github.com>
2021-07-15 11:09:15 +0700

    demo greeting

---
+++ testapigreeting/src/main/java/com/example/testapigreeting/TestapigreetingApplication.java
@@ -0,0 +1,13 @@
+package com.example.testapigreeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestapigreetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestapigreetingApplication.class, args);
+    }
+
+}
---
+++ testapigreeting/src/test/java/com/example/testapigreeting/TestapigreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testapigreeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestapigreetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
